d7609f1b59ab76583cc1aa8b7868b70da57faef0
MohammadReza Velayati <mohammadrv2003@gmail.com>
2024-07-18 18:22:39 +0330

    test post api with PyTest

---
+++ core/blog/tests/test_post_api.py
@@ -0,0 +1,64 @@
+from rest_framework.test import APIClient
+from django.urls import reverse
+import pytest
+from datetime import datetime
+from accounts.models import User
+
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture
+def common_user():
+    user = User.objects.create_user(email = "mohrez@gmail.com", password = "123", is_verified = True)
+    return user
+
+@pytest.mark.django_db
+class TestPostApi:
+
+    def test_post_api_response_200_status(self, api_client):
+        url = reverse("blog:api-v1:post-list")
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+
+
+    def test_create_post_response_401_status(self, api_client):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title" : "test",
+            "content" : "description",
+            "status" : True,
+            "published_date" : datetime.now()
+        }
+        response = api_client.post(url, data)
+        assert response.status_code == 401
+        
+
+    # def test_create_post_response_201_status(self, api_client, common_user):
+    #     url = reverse("blog:api-v1:post-list")
+    #     data = {
+    #         "title" : "test",
+    #         "content" : "description",
+    #         "status" : True,
+    #         "published_date" : datetime.now()
+    #     }
+    #     user = common_user
+    #     api_client.force_authenticate(user=user)
+    #     response = api_client.post(url, data)
+    #     assert response.status_code == 201
+
+
+
+    def test_create_post_invalid_data_response_400_status(self, api_client, common_user):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title" : "test",
+            "content" : "description",
+        }
+        user = common_user
+        api_client.force_authenticate(user=user)
+        response = api_client.post(url, data)
+        assert response.status_code == 400
